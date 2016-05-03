b984aca51cceebfe4b8115ff6272ceb08fa3ae73
John Rudolph <john.rudolph08@gmail.com>
2016-05-03 12:38:11 -0700

    final commit completed learning journals assignment

--- learning_journal/models/meta.py
+++ learning_journal/models/meta.py
@@ -10,3 +10,8 @@ from zope.sqlalchemy import ZopeTransactionExtension
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
+
+#changed structure of model to conform to: 
+#http://docs.pylonsproject.org/projects/pyramid-blogr/en/latest/basic_models.html
+
+#deleted out MyModel
